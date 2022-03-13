f7c85a5a218d45615b8d980cb48f7ac3e375600b
Leif <leif.k.boutin@gmail.com>
2022-03-13 15:06:20 +0100

    done?

---
+++ infos.js
@@ -0,0 +1,52 @@
+export default {
+	"me": {
+		"title": "Leif BOUTIN",
+		"article":
+			"Had a hard time at school, after a pretty useless bachelor's degree in English.. I managed to get a master's degree in digital marketing while being a professional gamer, decided to follow my passion for esport and work in live production. As I lost my job because of covid, I used that time to learn web developpment, a job and now hobby that I absolutely love. I'm always motivated to learn new things, I'm a team player, and I'm also pretty competitive! ",
+		"imgs": [
+			"./imgs/PL.jpg",
+			"./imgs/moi.jpg",
+			"./imgs/moi2.jpg",
+			"./imgs/moi3.jpg",
+			"./imgs/moi4.jpg",
+			"./imgs/moi5.jpg",
+			"./imgs/moi6.jpg",
+		],
+	},
+	"languages": {
+		"title": "Languages",
+		"article":
+			"As most learner, I started with HTML CSS & PHP, while PHP was my first language, I quickly found that javascript was incredible, by the amount of things it allows you to do, front or back. I then discovered Python which I grew to like a lot, then TypeScript which I first hated but quickly realised it's usefulness. Finally I wanted to give go a go and... it's a pretty incredible and powerfull language for any backend work!",
+		"imgs": [
+			"./imgs/html.png",
+			"./imgs/css.png",
+			"./imgs/javascript.png",
+			"./imgs/typescript.png",
+			"./imgs/python.png",
+			"./imgs/php.png",
+			"./imgs/golang.png",
+		],
+	},
+
+	"frameworks": {
+		"title": "Frameworks",
+		"article":
+			"Since I started with PHP, laravel was my very first framwork, and while I found how insane it really was, I grey tired of 'this' :D. With my discovery of Javascript, I dove head first into a world of frameworks, starting with VueJS which i really liked, then NodeJS & Express followed by NestJS or NextJS. After a little while I discovered React, React Native and ElectronJS and I realised that even though JS was not the grettest performer it allowed me to do anything! I've built mobile apps connected to Apis, desktop apps connected to live production to injected live informations with SocketIO... the possibilities were endless. I also realised how powerfull Django was and how fast it allowed a backend to be secured and deployed. I've build API, multiple backends with middleware and/or jwt, protected routes etc..",
+		"imgs": [
+			"./imgs/laravel.png",
+			"./imgs/vuejs.png",
+			"./imgs/reactjs.png",
+			"./imgs/react native.png",
+			"./imgs/django.png",
+			"./imgs/nestjs.png",
+			"./imgs/nextjs.png",
+			"./imgs/electronjs2.png",
+		],
+	},
+	"live-production": {
+		"title": "Live Production",
+		"article":
+			"I have a certain experience with live production as a whole. I've managed projects from the ground up, from preparing the events with clients, installing the sets, and actually running the show, either as director, director assistant or just overall technician. I have a ton of experience with vmix, the blackmagic studio environment or just plain obs studio.",
+		"imgs": ["./imgs/vmix.png", "./imgs/obs.png", "./imgs/blackmagic.png"],
+	},
+}
---
+++ script.js
@@ -0,0 +1,145 @@
+import data from "./infos.js"
+
+const article_title = document.querySelector(".article-title")
+const article_article = document.querySelector(".article-article")
+const article_image = document.querySelector(".article-image")
+const logo = document.querySelectorAll(".logo")
+const mobileButton = document.querySelector(".button")
+
+//slider mobile nav
+
+mobileButton.addEventListener("click", () => {
+	console.log("click")
+	let slider = document.querySelector(".slider")
+	slider.classList.add("show")
+	const cross = document.querySelectorAll(".cross")
+	cross.forEach((c) =>
+		c.addEventListener("click", () => {
+			slider.classList.remove("show")
+		})
+	)
+})
+console.log(logo)
+logo.forEach((l) => {
+	l.addEventListener("click", () => {
+		handleME()
+	})
+})
+//handle nav classic
+const lis = document.querySelectorAll("li")
+
+lis.forEach((li) => {
+	li.addEventListener("mouseover", () => {
+		li.nextElementSibling.classList.toggle(
+			"hide",
+			li.nextElementSibling.classList.contains("hide")
+		)
+	})
+
+	li.addEventListener("mouseout", () => {
+		li.nextElementSibling.classList.toggle(
+			"hide",
+			li.nextElementSibling.classList.contains("hide")
+		)
+	})
+	li.addEventListener("click", (e) => {
+		while (article_image.firstChild) {
+			article_image.firstChild.remove()
+		}
+		article_title.innerText = data[e.target.id].title
+		article_article.innerText = data[e.target.id].article
+
+		const rdm = data[e.target.id].imgs.sort((a, b) => 0.5 - Math.random())
+		rdm.forEach((src) => {
+			const img = document.createElement("img")
+
+			img.src = src
+			img.className =
+				e.target.id === "live-production"
+					? "article-images-production"
+					: "article-images"
+			article_image.append(img)
+		})
+		rdm.forEach((src) => {
+			const img = document.createElement("img")
+
+			img.src = src
+			img.className = "live-production"
+				? "article-images-production"
+				: "article-images"
+			article_image.append(img)
+		})
+	})
+})
+//handle mobile nav
+const lis2 = document.querySelectorAll("li")
+
+lis2.forEach((li) => {
+	li.addEventListener("mouseover", () => {
+		li.nextElementSibling.classList.toggle(
+			"hide",
+			!li.nextElementSibling.classList.contains("hide")
+		)
+	})
+
+	li.addEventListener("mouseout", () => {
+		console.log(li.nextElementSibling)
+		li.nextElementSibling.classList.toggle(
+			"hide",
+			!li.nextElementSibling.classList.contains("hide")
+		)
+	})
+	li.addEventListener("click", (e) => {
+		document.querySelector(".slider").classList.remove("show")
+		while (article_image.firstChild) {
+			article_image.firstChild.remove()
+		}
+		article_title.innerText = data[e.target.id].title
+		article_article.innerText = data[e.target.id].article
+
+		const rdm = data[e.target.id].imgs.sort((a, b) => 0.5 - Math.random())
+		rdm.forEach((src) => {
+			const img = document.createElement("img")
+
+			img.src = src
+			img.className =
+				e.target.id === "live-production"
+					? "article-images-production"
+					: "article-images"
+			article_image.append(img)
+		})
+		rdm.forEach((src) => {
+			const img = document.createElement("img")
+
+			img.src = src
+			img.className = "live-production"
+				? "article-images-production"
+				: "article-images"
+			article_image.append(img)
+		})
+	})
+})
+function handleME() {
+	while (article_image.firstChild) {
+		article_image.firstChild.remove()
+	}
+	article_title.innerText = data.me.title
+	article_article.innerText = data.me.article
+
+	const rdm = data.me.imgs.sort((a, b) => 0.5 - Math.random())
+	rdm.forEach((src) => {
+		const img = document.createElement("img")
+
+		img.src = src
+		img.className = "article-images"
+		article_image.append(img)
+	})
+	rdm.forEach((src) => {
+		const img = document.createElement("img")
+
+		img.src = src
+		img.className = "article-images"
+		article_image.append(img)
+	})
+}
+handleME()
